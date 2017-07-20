4053cb61f3f98586dd0986b32fb16ef512826349
Priscila Carvalho <priscila_silvacarvalho@hotmail.com>
2017-07-20 19:03:17 -0300

    Adicionando template sunset theme

---
+++ functions.php
@@ -0,0 +1,6 @@
+<?php 
+
+require get_template_directory() . '/inc/function-admin.php';
+
+
+?>
---
+++ inc/function-admin.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+
+@package sunsettheme
+
+	==================
+		ADMIN PAGE
+	==================
+
+*/
+
+	function sunse_add_admin_page(){
+
+		add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options');
+
+	}
