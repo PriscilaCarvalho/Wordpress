76e263d7e9594a7367246e8b7058babc695fbbfc
Priscila Carvalho <priscila_silvacarvalho@hotmail.com>
2017-07-20 20:19:17 -0300

    Adicionando ícone para o menu admin do tema no wordpress

--- inc/function-admin.php
+++ inc/function-admin.php
@@ -12,6 +12,13 @@
 
 	function sunse_add_admin_page(){
 
-		add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options');
+		add_menu_page('Sunset Theme Options', 'Sunset', 'manage_options', 'alecaddd-sunset', 'sunset_theme_create_page', 
+		get_template_directory_uri() . '/img/sunset-icon.png', 110 );
 
+	}
+
+	add_action( 'admin_menu', 'sunse_add_admin_page' );
+
+	function sunset_theme_create_page(){
+		echo "<h1>Sunset Theme Options</h1>";	
 	}
